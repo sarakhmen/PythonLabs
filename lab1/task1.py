2258c8e3243ba17da19f8182e8622bdf3374a118
staVjimr <74RrBzH2YP>
2021-09-30 15:49:57 +0300

    refactor

--- lab1/task1.py
+++ lab1/task1.py
@@ -6,5 +6,8 @@ if __name__ == '__main__':
     if len(sys.argv) != 4 or not sys.argv[1].isdigit() or not sys.argv[3].isdigit() or not sys.argv[2] in operations:
         print("Usage: number [+ | - | / | *) number")
     else:
-        result = eval(sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3])
-        print("Your result = " + str(result))
+        try:
+            result = eval(sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3])
+            print("Your result = " + str(result))
+        except ZeroDivisionError:
+            print("Division by zero")
